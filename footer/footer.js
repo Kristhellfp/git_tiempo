4824590ff831d3ba3670429463429ff77384058f
Iskrisss <klfpaz@scl.edu.gt>
2024-05-24 12:38:44 -0600

    modificaciones

--- footer/footer.js
+++ footer/footer.js
@@ -1,2 +1,2 @@
-let footer = document.querySelector(".footer")
-footer.innerHTML = `<div class="correo"></div><h3>scl.edu.gt</h3>`
+let footer = document.querySelector(".footer");
+footer.innerHTML = `<div class="correo"></div><h3>scl.edu.gt</h3>`;
