a0da8b2e7a0633a92566e51e279e52abe4093510
Iskrisss <klfpaz@scl.edu.gt>
2024-05-29 16:16:04 -0600

    Actualizacion header Kriss

---
+++ header/header.js
@@ -0,0 +1,2 @@
+let header_DOM = document.querySelector('.header')
+header_DOM.innerHTML = '<h1 class= "titulo">Comandos <span>GIT</span></h1> <a href="https://github.com/Kristhellfp/git_tiempo" class="btn-github">Github</a>';
